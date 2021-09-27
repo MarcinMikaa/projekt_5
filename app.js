476fbdd3965678e7cfd7a51ab2aa46384d30c4bb
Marcin <marcin.mika123@gmail.com>
2021-09-27 13:34:51 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const tasks = [
+    { id: 1, task: 'Evaluate the addition and deletion off user IDs.' },
+    { id: 2, task: 'Identify the implementation team.' },
+    { id: 3, task: 'Batch schedule download/process.' },
+    { id: 4, task: 'Monitor system performance and adjust hardware.' },
+    { id: 5, task: 'Evaluate the addition and deletion off user IDs.' },
+    { id: 6, task: 'Identify the implementation team.' },
+    { id: 7, task: 'Batch schedule download/process.' },
+]
