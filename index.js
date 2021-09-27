6e4a399b34620236d1bd06af769c1c8c22a67907
Marcin <marcin.mika123@gmail.com>
2021-09-27 13:50:57 +0200

    Update

--- index.js
+++ index.js
@@ -2,3 +2,20 @@ const express = require('express');
 const data = require('./tasks');
 
 const app = express();
+
+app.use(express.json());
+
+app.get('/tasks', (req, res) => {
+    res.json(data.tasks);
+});
+
+app.get('/tasks/:id', (req, res) => {
+    const id = req.params.id;
+
+    const data = data.tasks.find(element => element.id == id);
+
+    if (!user) {
+        res.status(404);
+    }
+
+});
