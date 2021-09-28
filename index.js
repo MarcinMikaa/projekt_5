594aec37e0868119d88e379d2e4d9aa83ec35e2e
Marcin <marcin.mika123@gmail.com>
2021-09-28 09:14:24 +0200

    Update

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+const e = require('express');
 const express = require('express');
 const data = require('./tasks');
 
 const app = express();
 
 app.use(express.json());
+
+const dataPut = {
+    name: 'Hardik Savani',
+    job: 'Blog Writer'
+};
 
 app.get('/tasks', (req, res) => {
     res.json(data.tasks);
@@ -12,10 +18,55 @@ app.get('/tasks', (req, res) => {
 app.get('/tasks/:id', (req, res) => {
     const id = req.params.id;
 
-    const data = data.tasks.find(element => element.id == id);
+    const task = data.tasks.find(element => element.id == id);
 
-    if (!user) {
+    if (!task) {
         res.status(404);
+        res.json({ error: "Task with this id doesn't exist"});
+        return;
     }
 
+    res.json(task);
 });
+
+app.post('/tasks', (req, res) => {
+    const message = req.body.message;
+
+    const existingTask = data.tasks.find(task => task.message === message);
+
+    if (existingTask) {
+        res.status(400);
+        res.json({ error: 'Task with given message already exists'});
+        return;
+    }
+
+    const task = {
+        id: Math.max(...data.tasks.map(task => task.id)) + 1,
+        message: message,
+    };
+
+    data.tasks.push(message);
+
+    res.status(201);
+    res.json(task);
+});
+
+app.put('/tasks/:id', (req, res) => {
+    const task = {
+        id: Math.max(...data.tasks.map(task => task.id)) + 1,
+        message: req.body.message,
+    };
+
+    data.tasks.push(task);
+
+    res.json(task);
+    console.log(data.tasks);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+
+});
+
+
+
+app.listen(3000);
--- tasks.js
+++ tasks.js
@@ -1,9 +1,13 @@
 const tasks = [
-    { id: 1, task: 'Evaluate the addition and deletion off user IDs.' },
-    { id: 2, task: 'Identify the implementation team.' },
-    { id: 3, task: 'Batch schedule download/process.' },
-    { id: 4, task: 'Monitor system performance and adjust hardware.' },
-    { id: 5, task: 'Evaluate the addition and deletion off user IDs.' },
-    { id: 6, task: 'Identify the implementation team.' },
-    { id: 7, task: 'Batch schedule download/process.' },
+    { id: 1, message: 'Evaluate the addition and deletion off user IDs.' },
+    { id: 2, message: 'Identify the implementation team.' },
+    { id: 3, message: 'Batch schedule download/process.' },
+    { id: 4, message: 'Monitor system performance and adjust hardware.' },
+    { id: 5, message: 'Evaluate the addition and deletion off user IDs.' },
+    { id: 6, message: 'Identify the implementation team.' },
+    { id: 7, message: 'Batch schedule download/process.' },
 ]
+
+module.exports = {
+    tasks,
+};
